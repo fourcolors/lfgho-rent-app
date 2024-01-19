4223cdd77a3bd907eacbd0ee5a6c34aafa670de4
Sterling <sterlingcobb@gmail.com>
2024-01-19 00:48:52 -0700

    additional style changes

--- src/components/Toggle/index.tsx
+++ src/components/Toggle/index.tsx
@@ -1,22 +1,37 @@
 import { motion } from "framer-motion";
-import { useState } from 'react';
+import { useEffect, useState } from "react";
 
+interface ToggleProps {
+  toggleState: (toggleState: boolean) => void;
+}
 
-export function Toggle() {
+export function Toggle({ toggleState }: ToggleProps) {
   const [toggle, setToggle] = useState(false);
 
   function handleToggle() {
-    setToggle(!toggle);
+    setToggle((toggle) => !toggle);
   }
 
+  useEffect(() => {
+    toggleState(toggle);
+  }, [toggle, toggleState]);
+
   return (
-    <div className='w-[200px] h-24 bg-gray-400  bg-opacity-40 flex justify-start rounded-[50px] p-2 cursor-pointer data-[toggled=true]:justify-end' data-toggled={toggle} onClick={handleToggle}>
-      <motion.div style={{
-        width: '80px',
-        height: '80px',
-        backgroundColor: 'white',
-        borderRadius: '50%',
-      }} layout transition={spring}/>
+    <div
+      className="w-[200px] h-24 bg-gray-400  bg-opacity-40 flex justify-start rounded-[50px] p-2 cursor-pointer data-[toggled=true]:justify-end"
+      data-toggled={toggle}
+      onClick={handleToggle}
+    >
+      <motion.div
+        style={{
+          width: "80px",
+          height: "80px",
+          backgroundColor: "white",
+          borderRadius: "50%",
+        }}
+        layout
+        transition={spring}
+      />
     </div>
   );
 }
@@ -24,5 +39,5 @@ export function Toggle() {
 const spring = {
   type: "spring",
   stiffness: 700,
-  damping: 30
-}
+  damping: 30,
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,40 @@
-import { Toggle } from '@/components/Toggle';
-import { ConnectKitButton, useSIWE } from 'connectkit';
+import { Toggle } from "@/components/Toggle";
+import { ConnectKitButton, useSIWE } from "connectkit";
+import { useState } from "react";
 
 export default function Home({ address }: { address?: string }) {
   const { data, isSignedIn, signOut, signIn } = useSIWE();
   console.log({ data, isSignedIn, signOut, signIn });
+
+  const [toggled, setToggled] = useState(false);
+
   return (
     <div className="flex  items-center justify-center min-h-screen py-2 flex-col text-center">
+      <div className="flex-1 flex-col items-center justify-end flex">
+        <p className="text-[72px] font-bold drop-shadow-3xl flex text-center">
+          RentFi
+        </p>
+        <Toggle toggleState={(state) => setToggled(state)} />
 
-      <div className='flex-1 flex-col items-center justify-end flex'>
-        <p className="text-[72px] font-bold drop-shadow-3xl flex text-center">RentFi</p>
-        <Toggle />
-        <p className='text-sm flex mt-3 font-bold'>Log in as Renter</p>
+        {toggled ? (
+          <p className="flex text-sm mt-3 font-bold">Log in as Renter</p>
+        ) : (
+          <p className="flex text-sm mt-3 font-bold ">Log in as Landlord</p>
+        )}
       </div>
 
-      <div className='flex flex-1 flex-col justify-end items-center py-5'>
-        <div className='bg-white bg-opacity-30 p-6 rounded-2xl flex'>
+      <div className="flex flex-1 flex-col justify-end items-center py-5">
+        <div className="bg-white bg-opacity-30 p-6 rounded-2xl flex">
           <ConnectKitButton />
         </div>
       </div>
     </div>
   );
 }
+const scaleAnimation = {
+  scale: [1, 2, 1], // Start at normal size, scale up to 1.5x, then back to normal
+  transition: {
+    duration: 0.5, // Duration for the entire animation sequence
+    ease: "easeInOut", // Ease function for the animation
+  },
+};
