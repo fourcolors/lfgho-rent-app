9a140788601672e81cedde8f1609f122c25bec45
Sterling <sterlingcobb@gmail.com>
2024-01-19 00:52:11 -0700

    add default landlord page

---
+++ src/pages/landlord/index.tsx
@@ -0,0 +1,11 @@
+export function LandlordHome() {
+  return (
+    <div className="flex-1 min-h-screen flex-col">
+      <h1>Welcome Landlord</h1>
+      <p>
+        This is the homepage for landlords. From here, you can navigate to
+        various landlord-specific features of the app.
+      </p>
+    </div>
+  );
+}
